f5af78311d26b8a3546b7b2660f7b1228ae3debc
Aman Kumar Chaurasiya <amanchaurasiya255@gmail.com>
2025-10-30 16:34:37 +0530

    fix vercel internal sever error

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -42,7 +42,7 @@ export async function POST(req: NextRequest) {
 
     response.cookies.set("token", token, {
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
       sameSite: "none",
       maxAge: 24 * 60 * 60,
       path: "/",
--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -45,7 +45,7 @@ export async function POST(req: NextRequest) {
 
     response.cookies.set("token", token, {
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
       sameSite: "none",
       maxAge: 24 * 60 * 60, // 1 day
       path: "/",
